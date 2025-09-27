b3c0ef41ad74f40efa23dd855db581221f2069ff
jopsco01 <83950678+jopsco01@users.noreply.github.com>
2025-09-27 11:44:29 +0200

    very good very nice multiplayer

---
+++ src/main/java/dev/casperrs/duckbongo/network/MoveOther.java
@@ -0,0 +1,9 @@
+package dev.casperrs.duckbongo.network;
+
+public class MoveOther {
+    public int targetId;
+    public float x;
+    public float y;
+    public boolean settled;
+    public MoveOther() {}
+}
